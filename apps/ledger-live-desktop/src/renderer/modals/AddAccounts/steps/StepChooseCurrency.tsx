d2a89ab5e04758e028d9e7e29981cfb8f14aea91
Luka Jeran <lukaw3d@gmail.com>
2025-04-30 21:11:59 +0000

    feature flags

--- apps/ledger-live-desktop/src/renderer/modals/AddAccounts/steps/StepChooseCurrency.tsx
+++ apps/ledger-live-desktop/src/renderer/modals/AddAccounts/steps/StepChooseCurrency.tsx
@@ -35,6 +35,8 @@ const listSupportedTokens = () =>
 const StepChooseCurrency = ({ currency, setCurrency }: StepProps) => {
   const mock = useEnv("MOCK");
 
+  const emerald = useFeature("currencyEmerald");
+  const sapphire = useFeature("currencySapphire");
   const aptos = useFeature("currencyAptos");
   const aptosTestnet = useFeature("currencyAptosTestnet");
   const axelar = useFeature("currencyAxelar");
@@ -97,6 +99,8 @@ const StepChooseCurrency = ({ currency, setCurrency }: StepProps) => {
 
   const featureFlaggedCurrencies = useMemo(
     (): Partial<Record<CryptoCurrencyId, Feature<unknown> | null>> => ({
+      emerald,
+      sapphire,
       aptos,
       aptos_testnet: aptosTestnet,
       axelar,
@@ -158,6 +162,8 @@ const StepChooseCurrency = ({ currency, setCurrency }: StepProps) => {
       babylon,
     }),
     [
+      emerald,
+      sapphire,
       aptos,
       aptosTestnet,
       axelar,
